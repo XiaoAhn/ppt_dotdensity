6e17f8ed877606be649d787fec05968cc189209a
Angela Wang <xiaoyanw@uchicago.edu>
2021-03-20 00:31:57 -0500

    update

--- mapbox/style.js
+++ mapbox/style.js
@@ -7,11 +7,11 @@ var styleJSON = {
         "intensity": 0.2
     },
     "sources": {
-        "stateData_0": {
-            "type": "geojson",
-            "data": json_stateData_0
-        }
-                    ,
+        "OSMStandard_0": {
+            "type": "raster",
+            "tiles": ["http://tile.openstreetmap.org/{z}/{x}/{y}.png"],
+            "tileSize": 256
+        },
         "Randompoints_1": {
             "type": "geojson",
             "data": json_Randompoints_1
@@ -34,19 +34,16 @@ var styleJSON = {
             }
         },
         {
-            "id": "lyr_stateData_0_0",
-            "type": "line",
-            "source": "stateData_0",
-            "layout": {},
-            "paint": {'line-width': 0.96, 'line-opacity': 1.0, 'line-color': '#a2b1b0'}
-        }
-,
+            "id": "lyr_OSMStandard_0_0",
+            "type": "raster",
+            "source": "OSMStandard_0"
+        },
         {
             "id": "lyr_Randompoints_1_0",
             "type": "circle",
             "source": "Randompoints_1",
             "layout": {},
-            "paint": {'circle-radius': ['/', 0.10714285714285714, 2], 'circle-color': '#487bb6', 'circle-opacity': 1.0, 'circle-stroke-width': 1.4285714285714286, 'circle-stroke-color': '#325780'}
+            "paint": {'circle-radius': ['/', 0.21428571428571427, 2], 'circle-color': '#487bb6', 'circle-opacity': 1.0, 'circle-stroke-width': 1.4285714285714286, 'circle-stroke-color': '#325780'}
         }
 ,
         {
@@ -54,7 +51,7 @@ var styleJSON = {
             "type": "circle",
             "source": "Randompoints_2",
             "layout": {},
-            "paint": {'circle-radius': ['/', 0.05357142857142857, 2], 'circle-color': '#db1e2a', 'circle-opacity': 1.0, 'circle-stroke-width': 1.4285714285714286, 'circle-stroke-color': '#801119'}
+            "paint": {'circle-radius': ['/', 0.06785714285714285, 2], 'circle-color': '#db1e2a', 'circle-opacity': 1.0, 'circle-stroke-width': 1.4285714285714286, 'circle-stroke-color': '#801119'}
         }
 ],
 }
